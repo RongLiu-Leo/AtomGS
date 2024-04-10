adfcabea8246038ff5c39ff758209f273282a377
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2024-04-09 22:24:03 -0400

    Revert "update viewer"
    This reverts commit 5b3f355d6c7db8d93850974257b3c7b4126b1d9c.

--- gaussian_renderer/network_gui.py
+++ gaussian_renderer/network_gui.py
@@ -32,9 +32,9 @@ def init(wish_host, wish_port):
     listener.listen()
     listener.settimeout(0)
 
-def send_json_data(conn, data):
+def send_render_items(conn, string_list):
     # Serialize the list of strings to JSON
-    serialized_data = json.dumps(data)
+    serialized_data = json.dumps(string_list)
     # Convert the serialized data to bytes
     bytes_data = serialized_data.encode('utf-8')
     # Send the length of the serialized data first
@@ -48,10 +48,9 @@ def try_connect(render_items):
         conn, addr = listener.accept()
         print(f"\nConnected by {addr}")
         conn.settimeout(None)
-        send_json_data(conn, render_items)
+        send_render_items(conn, render_items)
     except Exception as inst:
         pass
-        # raise inst
             
 def read():
     global conn
@@ -60,16 +59,16 @@ def read():
     message = conn.recv(messageLength)
     return json.loads(message.decode("utf-8"))
 
-def send(message_bytes, verify, metrics):
+def send(message_bytes, verify):
     global conn
     if message_bytes != None:
         conn.sendall(message_bytes)
     conn.sendall(len(verify).to_bytes(4, 'little'))
     conn.sendall(bytes(verify, 'ascii'))
-    send_json_data(conn, metrics)
 
 def receive():
     message = read()
+
     width = message["resolution_x"]
     height = message["resolution_y"]
 
@@ -80,6 +79,8 @@ def receive():
             fovx = message["fov_x"]
             znear = message["z_near"]
             zfar = message["z_far"]
+            do_shs_python = bool(message["shs_python"])
+            do_rot_scale_python = bool(message["rot_scale_python"])
             keep_alive = bool(message["keep_alive"])
             scaling_modifier = message["scaling_modifier"]
             world_view_transform = torch.reshape(torch.tensor(message["view_matrix"]), (4, 4)).cuda()
@@ -93,6 +94,6 @@ def receive():
             print("")
             traceback.print_exc()
             # raise e
-        return custom_cam, do_training, keep_alive, scaling_modifier, render_mode
+        return custom_cam, do_training, do_shs_python, do_rot_scale_python, keep_alive, scaling_modifier, render_mode
     else:
-        return None, None, None, None, None
+        return None, None, None, None, None, None, None
