b9198b0a8b43d7bbb7b51d773c631aad81e30aaa
Rong Liu <Rong_Leo@outlook.com>
2024-04-29 01:35:43 -0700

    building up

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -15,115 +15,41 @@ document.addEventListener('DOMContentLoaded', domReady);
             });
         }
 
-        function largeSceneEvent(idx) {
+        function objectSceneEvent(idx) {
             let dics = document.querySelectorAll('.b-dics')[0]
             let sections = dics.getElementsByClassName('b-dics__section')
             let imagesLength = 3
             for (let i = 0; i < imagesLength; i++) {
                 let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                let parts = image.src.split('/').slice(0, -1);
                 switch (idx) {
                     case 0:
-                        image.src = 'assets/img/nvidia/';
+                        parts[parts.length-2] = 'bicycle'
+                        image.src = parts.join('/')
                         break;
                     case 1:
-                        image.src = 'assets/img/jhu/';
+                        parts[parts.length-2] = 'kitchen'
+                        image.src = parts.join('/')
                         break;
                     case 2:
-                        image.src = 'assets/img/Barn/';
+                        image.src = 'static/images/stump/';
                         break;
                     case 3:
-                        image.src = 'assets/img/Caterpillar/';
+                        image.src = 'static/images/garden/';
                         break;
                     case 4:
-                        image.src = 'assets/img/Courthouse/';
-                        break;
-                    case 5:
-                        image.src = 'assets/img/Ignatius/';
-                        break;
-                    case 6:
-                        image.src = 'assets/img/Meetingroom/';
-                        break;
-                    case 7:
-                        image.src = 'assets/img/Truck/';
-                        break;
+                        image.src = 'static/images/truck/';
+                        break;   
                 }
                 switch (i) {
                     case 0:
-                        image.src = image.src + '/rgb.png';
+                        image.src = image.src + '/gs.png';
                         break;
                     case 1:
-                        image.src = image.src + '/mesh.png';
+                        image.src = image.src + '/ours.png';
                         break;
                     case 2:
-                        image.src = image.src + '/normal.png';
-                        break;
-                }
-            }
-
-            scene_list = document.getElementById("large-scale-recon-1").children;
-            for (let i = 0; i < scene_list.length; i++) {
-                if (idx == i) {
-                    scene_list[i].children[0].className = "nav-link active"
-                }
-                else {
-                    scene_list[i].children[0].className = "nav-link"
-                }
-            }
-            scene_list = document.getElementById("large-scale-recon-2").children;
-            for (let i = 0; i < scene_list.length; i++) {
-                if (idx == i+2) {
-                    scene_list[i].children[0].className = "nav-link active"
-                }
-                else {
-                    scene_list[i].children[0].className = "nav-link"
-                }
-            }
-        }
-
-        function objectSceneEvent(idx) {
-            let dics = document.querySelectorAll('.b-dics')[0]
-            let sections = dics.getElementsByClassName('b-dics__section')
-            let imagesLength = 4
-            for (let i = 0; i < imagesLength; i++) {
-                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
-                switch (idx) {
-                    case 0:
-                        image.src = 'resources/360_stmt_supp/bicycle_0';
-                        break;
-                    case 1:
-                        image.src = 'resources/360_stmt_supp/bonsai_12';
-                        break;
-                    case 2:
-                        image.src = 'resources/360_stmt_supp/counter_19';
-                        break;
-                    case 3:
-                        image.src = 'resources/360_stmt_supp/flowers_8';
-                        break;
-                    case 4:
-                        image.src = 'resources/360_stmt_supp/garden_1';
-                        break;
-                    case 5:
-                        image.src = 'resources/360_stmt_supp/kitchen_0';
-                        break;
-                    case 6:
-                        image.src = 'resources/360_stmt_supp/stump_0';
-                        break;
-                    case 7:
-                        image.src = 'resources/360_stmt_supp/treehill_0';
-                        break;    
-                }
-                switch (i) {
-                    case 0:
-                        image.src = image.src + '_ewa.jpg';
-                        break;
-                    case 1:
-                        image.src = image.src + '_ours.jpg';
-                        break;
-                    case 2:
-                        image.src = image.src + '_upgt.jpg';
-                        break;
-                    case 3:
-                        image.src = image.src + '_gt.jpg';
+                        image.src = image.src + '/sugar.png';
                         break;
 
                 }
@@ -139,50 +65,26 @@ document.addEventListener('DOMContentLoaded', domReady);
                 }
             }
         }
-
-        function ablation3DEvent(idx) {
-            let dics = document.querySelectorAll('.b-dics')[1]
+        function rgbNormalEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[0]
             let sections = dics.getElementsByClassName('b-dics__section')
-            let imagesLength = 4
+            let imagesLength = 3
             for (let i = 0; i < imagesLength; i++) {
                 let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                let parts = image.src.split('/');
                 switch (idx) {
                     case 0:
-                        image.src = 'resources/360_stmt_ablation/bicycle_0';
+                        parts[parts.length-2] = 'rgb'
+                        image.src = parts.join('/')
                         break;
                     case 1:
-                        image.src = 'resources/360_stmt_ablation/bicycle_3';
-                        break;
-                    case 2:
-                        image.src = 'resources/360_stmt_ablation/bicycle_5';
-                        break;
-                    case 3:
-                        image.src = 'resources/360_stmt_ablation/garden_0';
-                        break; 
-                    case 4:
-                        image.src = 'resources/360_stmt_ablation/garden_1';
-                        break;
-                    case 5:
-                        image.src = 'resources/360_stmt_ablation/treehill_9';
-                        break; 
-                }
-                switch (i) {
-                    case 0:
-                        image.src = image.src + '_no3d.jpg';
-                        break;
-                    case 1:
-                        image.src = image.src + '_ours.jpg';
-                        break;
-                    case 2:
-                        image.src = image.src + '_upgt.jpg';
-                        break;
-                    case 3:
-                        image.src = image.src + '_gt.jpg';
+                        parts[parts.length-2] = 'normal'
+                        image.src = parts.join('/')
                         break;
                 }
             }
 
-            let scene_list = document.getElementById("ablation-3d-filter").children;
+            let scene_list = document.getElementById("object-rgb-normal").children;
             for (let i = 0; i < scene_list.length; i++) {
                 if (idx == i) {
                     scene_list[i].children[0].className = "nav-link active"
